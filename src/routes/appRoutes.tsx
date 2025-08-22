2678800f07dd57b0fbe87bbf713e5044e4429156
vorlaxen <mrkaygusuzhakan@gmail.com>
2025-08-22 23:56:59 +0300

    sdsdasad

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Test from '@/pages/Test/Test'
 import MainLayout from '@/layouts/HomeLayout'
+import NotFound from '@/pages/Status/NotFound/NotFound'
+import Unauthorized from '@/pages/Status/Unauthorized/Unauthorized'
 
 const AppRoutes = () => (
   <Routes>
     <Route path='/' element={<MainLayout />}>
       <Route index element={<Home />} />
+      <Route path='/status/404' element={<NotFound />} />
+      <Route path='/status/401' element={<Unauthorized />} />
     </Route>
+
     <Route path='/test' element={<Test />} />
+    <Route path='*' element={<Navigate to="/status/404" replace />} />
   </Routes>
 )
 
